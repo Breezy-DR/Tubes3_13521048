28c52e05c620074877cf330171cd451afbfbf960
zidane-itb <16521420@mahasiswa.itb.ac.id>
2023-05-04 23:21:35 +0700

    commit example

--- src/frontend/tubes3/src/component/ChatForm.js
+++ src/frontend/tubes3/src/component/ChatForm.js
@@ -11,7 +11,31 @@ function ChatForm() {
     event.preventDefault();
     // Send input (message) to database here
     // ....
+    sessionStorage.getItem("session_id");
+    sendGetRequestWithJsonBody("http://localhost:8080/question",
+        {"question": event.target.value, "search_algorithm":"kmp", "session_id": "7bbf04e082"},
+        x)
   };
+
+  function x(json) {
+    sessionStorage.setItem("session_id", "example");
+    // var myObject = JSON.parse(json)
+    console.log(json["session_id"])
+
+  }
+
+  function sendGetRequestWithJsonBody(url, data, callback) {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(json => callback(json))
+        .catch(error => console.error(error));
+  }
 
   const handleKeyDown = (event) => {
     if(event.key === 'Enter') {
@@ -19,28 +43,9 @@ function ChatForm() {
     }
   }
 
-  const sendData = async () => {
-    const url = "ec2-54-169-32-134.ap-southeast-1.compute.amazonaws.com:8080/question/";
-    const data = {
-      question: "ping? namx 5 nama ikan? 2*9^(7-2); 7 nama ikan; sebutkan 7 nama ikan; 2023/11/12?pong",
-      search_algorithm: "bm",
-      session_id: "7bbf04e082"
-    };
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    };
-    const response = await fetch(url, options);
-    const jsonResponse = await response.json();
-    console.log(jsonResponse);
-  };
-
   return (
     <div className='ChatForm'>
-      <form onSubmit={sendData}>
+      <form onSubmit={handleSubmit}>
       <input type='text' name="message" placeholder='Masukkan pertanyaan Anda (press enter to submit)' onChange={handleChange} onKeyDown={handleKeyDown}/>
       </form>
     </div>
