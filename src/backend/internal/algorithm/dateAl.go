1cdd373185a7021f8884c6bdc93d298cf441760f
zidane-itb <16521420@mahasiswa.itb.ac.id>
2023-05-05 21:31:39 +0700

    finalization

--- src/backend/internal/algorithm/dateAl.go
+++ src/backend/internal/algorithm/dateAl.go
@@ -2,6 +2,7 @@ package algorithm
 
 import (
 	"backend/internal/util"
+	"errors"
 	"fmt"
 	"math"
 	"strconv"
@@ -11,13 +12,20 @@ import (
 func GetDayDate(dateStr string) string {
 
 	now := "0001-01-01"
-	num := int(math.Mod(math.Abs(float64(getX(dateStr)-getX(now))), 7))
+	dstr, err := getX(dateStr)
+
+	if err != nil {
+		return err.Error()
+	}
+
+	n, _ := getX(now)
+	num := int(math.Mod(math.Abs(float64(dstr-n)), 7))
 	var day = []string{"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"}
 	return day[num]
 
 }
 
-func getX(dateStr string) int {
+func getX(dateStr string) (int, error) {
 	var days = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 	var year float64
 	var month float64
@@ -51,6 +59,14 @@ func getX(dateStr string) int {
 		day, _ = strconv.ParseFloat(dateList[0], 64)
 	}
 
+	if month > 12 || month < 1 {
+		return 0, errors.New("invalid date")
+	} else if day > float64(days[int(month)-1]) || day < 0 {
+		return 0, errors.New("invalid date")
+	} else if year < 1 {
+		return 0, errors.New("invalid date")
+	}
+
 	dayInY := int(year)*365 + int(math.Floor(year/4.0)) - int(math.Floor(year/100)) + int(math.Floor(year/400))
 	dayInM := 0
 	for i := 0; i < int(month)-1; i++ {
@@ -59,5 +75,5 @@ func getX(dateStr string) int {
 	dayInD := day
 	fmt.Println(dayInY, dayInM, dayInD)
 
-	return dayInY + dayInM + int(dayInD)
+	return dayInY + dayInM + int(dayInD), nil
 }
